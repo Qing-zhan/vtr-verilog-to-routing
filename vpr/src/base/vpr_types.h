114760a03f41de70de2e88fa00c3a04429804078
Mustafa Abbas <mustafa.abbas@bucknell.edu>
2018-04-30 13:50:14 -0400

    Added VPR general option to route the clock - currently this option does nothing; it is only stored in the vpr setting struct - there is no unit test created for validating this option

--- vpr/src/base/vpr_types.h
+++ vpr/src/base/vpr_types.h
@@ -29,6 +29,7 @@
 #include "atom_netlist_fwd.h"
 #include "clustered_netlist_fwd.h"
 #include "constant_nets.h"
+#include "clock_modeling.h"
 
 #include "vtr_assert.h"
 #include "vtr_ndmatrix.h"
@@ -1135,6 +1136,7 @@ struct t_vpr_setup {
 	t_power_opts PowerOpts;
     std::string device_layout;
     e_constant_net_method constant_net_method; //How constant nets should be handled
+    e_clock_modeling_method clock_modeling_method; //How clocks should be handled
 };
 
 class RouteStatus {
